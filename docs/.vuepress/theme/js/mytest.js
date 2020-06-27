7bd06029749b4d459e7369ccf5ea7cc2ddd7ca81
liuchenyang <897418643@qq.com>
2020-06-27 21:28:12 +0800

    Promise改为async/await

--- docs/.vuepress/theme/js/mytest.js
+++ docs/.vuepress/theme/js/mytest.js
@@ -1,9 +1,12 @@
-Promise.resolve().then(() => {
+async function jq() {
     const s = document.createElement('script');
     s.type = 'text/javascript';
     s.src = 'https://lib.baomitu.com/jquery/3.4.1/jquery.min.js';
     document.body.appendChild(s);
-}).then(()=> {
+}
+
+!(async function() {
+    await jq();
     let m = document.createElement('script');
     m.type = 'text/javascript';
     m.src = 'https://player.lmih.cn/player/js/player.js';
@@ -11,4 +14,18 @@ Promise.resolve().then(() => {
     m.setAttribute("key", "159325101761");
     m.setAttribute("m", 1);
     document.body.appendChild(m);
-})
+})()
+// Promise.resolve().then(() => {
+//     const s = document.createElement('script');
+//     s.type = 'text/javascript';
+//     s.src = 'https://lib.baomitu.com/jquery/3.4.1/jquery.min.js';
+//     document.body.appendChild(s);
+// }).then(()=> {
+//     let m = document.createElement('script');
+//     m.type = 'text/javascript';
+//     m.src = 'https://player.lmih.cn/player/js/player.js';
+//     m.setAttribute("id", "myhk");
+//     m.setAttribute("key", "159325101761");
+//     m.setAttribute("m", 1);
+//     document.body.appendChild(m);
+// })
