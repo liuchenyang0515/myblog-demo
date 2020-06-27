95b362abe082939875413f4d39631800e3feb3a5
liuchenyang <897418643@qq.com>
2020-06-27 19:21:54 +0800

    用Promise控制回调

--- docs/.vuepress/theme/js/mytest.js
+++ docs/.vuepress/theme/js/mytest.js
@@ -1,9 +1,9 @@
-const s = document.createElement('script');
-s.type = 'text/javascript';
-s.src = 'https://lib.baomitu.com/jquery/3.4.1/jquery.min.js';
-document.body.appendChild(s);
-
-setTimeout(() => {
+Promise.resolve().then(() => {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://lib.baomitu.com/jquery/3.4.1/jquery.min.js';
+    document.body.appendChild(s);
+}).then(()=> {
     let m = document.createElement('script');
     m.type = 'text/javascript';
     m.src = 'https://player.lmih.cn/player/js/player.js';
@@ -11,4 +11,4 @@ setTimeout(() => {
     m.setAttribute("key", "159325101761");
     m.setAttribute("m", 1);
     document.body.appendChild(m);
-}, 1000);
+})
