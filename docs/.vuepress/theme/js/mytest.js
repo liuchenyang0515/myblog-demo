d85d658707a9ed4eaddf8b67f5f71b5bbd45330a
liuchenyang <897418643@qq.com>
2020-06-27 22:03:43 +0800

    尝试减少加载jq失败的情形

--- docs/.vuepress/theme/js/mytest.js
+++ docs/.vuepress/theme/js/mytest.js
@@ -7,7 +7,7 @@ async function jq() {
 
 !(async function() {
     await jq();
-    setTimeout(() => {
+    window.onload=()=>{
         let m = document.createElement('script');
         m.type = 'text/javascript';
         m.src = 'https://player.lmih.cn/player/js/player.js';
@@ -15,7 +15,7 @@ async function jq() {
         m.setAttribute("key", "159325101761");
         m.setAttribute("m", 1);
         document.body.appendChild(m);
-    }, 500); // 前一个文件始终有可能加载不出来，必须延时策略
+    }
 })()
 // Promise.resolve().then(() => {
 //     const s = document.createElement('script');
