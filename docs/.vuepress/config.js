5b395e14af1bea3c8558acc4e012e994f03d9142
liuchenyang <897418643@qq.com>
2020-07-03 12:45:39 +0800

    测试评论

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,13 +16,13 @@ module.exports = {
     plugins: {
       '@vssue/vuepress-plugin-vssue': {
         platform: 'github', //v3的platform是github，v4的是github-v4
-        locale: 'zh', //语言
+        // locale: 'zh', //语言
         // 其他的 Vssue 配置
         owner: 'liuchenyang0515', //github账户名
         repo: 'liuchenyang0515.github.io', //github一个项目的名称
         clientId: 'bb4ff4ad145588ae166f',//注册的Client ID
         clientSecret: '669ec1391af7e66101ebcbf6e38ec61dd4ade717',//注册的Client Secret
-        autoCreateIssue:true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+        // autoCreateIssue:true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
       },
     },
   };
